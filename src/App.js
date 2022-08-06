8523b79cb91f2b728e97dbe9bd10c941be802993
Luke Lai <lukelai.dev@gmail.com>
2022-08-06 11:45:11 +0100

    Add basic game logic & Reset Function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Board from "./components/Board";
 import Square from "./components/Square";
+import resetIcon from "./assets/icon-restart.svg";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 
 const defaultSquares = () => new Array(9).fill(null);
 
@@ -18,11 +19,44 @@ const lines = [
 
 function App() {
   const [squares, setSquares] = useState(defaultSquares());
+  const [winner, setWinner] = useState(null);
 
   //computer move
   useEffect(() => {
     const isComputerTurn =
       squares.filter((square) => square !== null).length % 2 === 1;
+
+    //Checking if any line made from a, b, c combinations
+    const linesThatAre = (a, b, c) => {
+      return lines.filter((squareIndexes) => {
+        const squareValues = squareIndexes.map((index) => squares[index]);
+        return (
+          JSON.stringify([a, b, c].sort()) ===
+          JSON.stringify(squareValues.sort())
+        );
+      });
+    };
+
+    //Find any empty index
+    const emptyIndexes = squares
+      .map((square, index) => (square === null ? index : null))
+      .filter((val) => val !== null);
+
+    const playerWon = linesThatAre("x", "x", "x").length > 0;
+    const computerWon = linesThatAre("o", "o", "o").length > 0;
+    const drawGame = squares.filter((index) => index !== null).length === 9;
+
+    //Set winner
+    if (playerWon) {
+      setWinner("x");
+    }
+    if (computerWon) {
+      setWinner("o");
+    }
+
+    if (drawGame) {
+      setWinner("draw");
+    }
 
     //Computer playing
     const putComputerAt = (index) => {
@@ -32,10 +66,43 @@ function App() {
     };
 
     //If computer turn
-    if (isComputerTurn) {
-      const emptyIndexes = squares
-        .map((square, index) => (square === null ? index : null))
-        .filter((val) => val !== null);
+    if (isComputerTurn && !winner) {
+      const winningLines = linesThatAre("o", "o", null);
+
+      if (winningLines.length > 0) {
+        const winPosition = winningLines[0].filter(
+          (index) => squares[index] === null
+        )[0];
+        setTimeout(() => {
+          putComputerAt(winPosition);
+        }, "700");
+        return;
+      }
+
+      const linesToBlock = linesThatAre("x", "x", null);
+
+      if (linesToBlock.length > 0) {
+        const blockPosition = linesToBlock[0].filter(
+          (index) => squares[index] === null
+        )[0];
+        setTimeout(() => {
+          putComputerAt(blockPosition);
+        }, "700");
+        return;
+      }
+
+      const linesToContinue = linesThatAre("o", null, null);
+
+      if (linesToContinue.length > 0) {
+        const continuePosition = linesToContinue[0].filter(
+          (index) => squares[index] === null
+        )[0];
+        setTimeout(() => {
+          putComputerAt(continuePosition);
+        }, "700");
+        return;
+      }
+
       const randomIndex =
         emptyIndexes[Math.ceil(Math.random() * emptyIndexes.length)];
 
@@ -50,24 +117,40 @@ function App() {
     const isPlayerTurn =
       squares.filter((square) => square !== null).length % 2 === 0;
 
-    if (isPlayerTurn) {
+    if (isPlayerTurn && !winner) {
       let newSquares = squares;
       newSquares[index] = "x";
       setSquares([...newSquares]);
     }
   };
+
+  //reset the game
+  const handleReset = () => {
+    setSquares(defaultSquares());
+    setWinner(null);
+  };
   return (
-    <main>
-      <Board>
-        {squares.map((square, index) => (
-          <Square
-            x={square === "x" ? 1 : 0}
-            o={square === "o" ? 1 : 0}
-            onClick={() => handleSquareClick(index)}
-          />
-        ))}
-      </Board>
-    </main>
+    <Fragment>
+      <header>
+        <button onClick={handleReset}>
+          <img src={resetIcon} alt="reset" />
+        </button>
+      </header>
+      <main>
+        <Board>
+          {squares.map((square, index) => (
+            <Square
+              x={square === "x" ? 1 : 0}
+              o={square === "o" ? 1 : 0}
+              onClick={() => handleSquareClick(index)}
+            />
+          ))}
+        </Board>
+        {winner === "x" && <div>You Won!</div>}
+        {winner === "o" && <div>Computer Won!</div>}
+        {winner === "draw" && <div>Draw!</div>}
+      </main>
+    </Fragment>
   );
 }
 
