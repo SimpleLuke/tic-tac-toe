e0f9f40512cd59ab555855510189cc91c7f39997
Luke Lai <lukelai.dev@gmail.com>
2022-08-06 10:59:15 +0100

    Add function of player and computer take turn play x & o

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import Board from "./components/Board";
 import Square from "./components/Square";
 
+import { useState, useEffect } from "react";
+
+const defaultSquares = () => new Array(9).fill(null);
+
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function App() {
+  const [squares, setSquares] = useState(defaultSquares());
+
+  //computer move
+  useEffect(() => {
+    const isComputerTurn =
+      squares.filter((square) => square !== null).length % 2 === 1;
+
+    //Computer playing
+    const putComputerAt = (index) => {
+      let newSquares = squares;
+      newSquares[index] = "o";
+      setSquares([...newSquares]);
+    };
+
+    //If computer turn
+    if (isComputerTurn) {
+      const emptyIndexes = squares
+        .map((square, index) => (square === null ? index : null))
+        .filter((val) => val !== null);
+      const randomIndex =
+        emptyIndexes[Math.ceil(Math.random() * emptyIndexes.length)];
+
+      setTimeout(() => {
+        putComputerAt(randomIndex);
+      }, "700");
+    }
+  }, [squares]);
+
+  //player clicking
+  const handleSquareClick = (index) => {
+    const isPlayerTurn =
+      squares.filter((square) => square !== null).length % 2 === 0;
+
+    if (isPlayerTurn) {
+      let newSquares = squares;
+      newSquares[index] = "x";
+      setSquares([...newSquares]);
+    }
+  };
   return (
     <main>
       <Board>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        {squares.map((square, index) => (
+          <Square
+            x={square === "x" ? 1 : 0}
+            o={square === "o" ? 1 : 0}
+            onClick={() => handleSquareClick(index)}
+          />
+        ))}
       </Board>
     </main>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,18 @@
-const Square = () => {
-  return <div className="square"></div>;
+import crossIcon from "../assets/icon-x.svg";
+import circleIcon from "../assets/icon-o.svg";
+
+const Square = (props) => {
+  return (
+    <div className="square" {...props}>
+      {props.x ? (
+        <img src={crossIcon} alt="cross icon" />
+      ) : props.o ? (
+        <img src={circleIcon} alt="circle icon" />
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default Square;
