f70ddd9c5284591ff58b7fb778885e762bd4855f
Luke Lai <lukelai.dev@gmail.com>
2022-08-10 19:11:31 +0100

    Fix stage bug to home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home/Home";
 import { Fragment, useState, useEffect } from "react";
 
 function App() {
-  const [mode, setMode] = useState(localStorage.getItem("stage"));
+  const [mode, setMode] = useState(localStorage.getItem("stage") || "home");
   const [playerMark, setPlayerMark] = useState(
     mode === "home" ? "x" : localStorage.getItem("playerMark")
   );
